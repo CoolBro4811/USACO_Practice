cffc54727de5ef76af6f6e2548eb83b29acbfeec
CoolBro4811 <121374566+CoolBro4811@users.noreply.github.com>
2024-11-22 18:57:19 +0000

    fixing files + directories, started LM Academy

---
+++ Code Quest Academy/HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void printOutput();
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        printOutput();
+    }
+
+    return 0;
+}
+
+void printOutput() {
+    std::string temp;
+    std::cin >> temp;
+    std::cout << temp;
+    return;
+}
