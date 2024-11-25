51bea6f6c8d961b507b95b0fa0b5acca744885cd
CoolBro4811 <piano4life010@gmail.com>
2024-11-25 02:34:25 -0800

    working on are you a spy, using sets and std::all_of for unique letters in second sentence are in the set of all unique letters from first sentence

--- Code Quest Academy/AreYouASpy.cpp
+++ Code Quest Academy/AreYouASpy.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 
 std::string removePunctuation(const std::string &result);
+std::string trim(const std::string &str);
 void getOutput(); 
 
 int main() {
@@ -24,30 +25,51 @@ std::string removePunctuation(const std::string &result) {
 	return input;
 }
 
+std::string trim(const std::string &str) {
+	size_t start = str.find_first_not_of(" 1234567890\t\n\r");
+    	size_t end = str.find_last_not_of(" 1234567890\t\n\r");
+    	return (start == std::string::npos) ? "" : str.substr(start, end - start + 1);
+}
+
 void getOutput() {
 	std::string input;
 	getline(std::cin, input);
+	
+	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
 
 	std::string firstSentence { input.substr(0, input.find("|")) };
 	std::string secondSentence { input.substr(input.find("|")+1) };
 
+	std::cout << firstSentence << secondSentence << "\n";
+
+	firstSentence = trim(firstSentence);
+	secondSentence = trim(secondSentence);
+
 	firstSentence = removePunctuation(firstSentence);
 	secondSentence = removePunctuation(secondSentence);
-
-	bool containsLetter { false };
-	for (int i = 0; i < firstSentence.length(); i++) {
-		for (int j = 0; j < secondSentence.length(); j++) {
-			if (firstSentence[i] == secondSentence[j]) {
-				containsLetter = true; 
-				break;
-			}
-		}
-		if (!containsLetter) {
-			std::cout << "You're not a secret agent!";
-			return;
-		}
-		containsLetter = false;
-	}
 	
-	std::cout << "That's my secret contact!";
+	std::sort(firstSentence.begin(), firstSentence.end());
+	std::sort(secondSentence.begin(), secondSentence.end());
+
+	
+	std::set<char> firstSet(firstSentence.begin(), firstSentence.end());
+	firstSentence = std::string(firstSet.begin(), firstSet.end());
+
+	std::set<char> secondSet(secondSentence.begin(), secondSentence.end());
+	secondSentence = std::string(secondSet.begin(), secondSet.end());
+    
+	bool isValid = std::all_of(secondSet.begin(), secondSet.end(), [&](char c) {
+        	return firstSet.count(c);
+    	});
+
+	std::cout << firstSentence << secondSentence << "\n";
+	
+	std::cout << firstSentence << secondSentence << "\n";
+
+	if (isValid) {
+		std::cout << "That's my secret contact!" << "\n";
+	}	
+	else {
+		std::cout << "You're not a secret agent!" << "\n";
+	}
 }
