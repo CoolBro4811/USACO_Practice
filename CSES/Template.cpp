cc575379a57d396cc98e0e7896e2fdbec10c628d
CoolBro4811 <piano4life010@gmail.com>
2024-11-25 13:21:31 -0800

    also created a template file

---
+++ CSES/Template.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <bits/stdc++.h>
+
+void getOutput();
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+	
+	int n;
+	std::cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		getOutput();
+	}
+
+	return 0;
+}
+
+void getOutput() {
+
+}
+
