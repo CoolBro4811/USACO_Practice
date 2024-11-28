f59fdfdf7128f002b924c1dafd4457ff219a1154
CoolBro4811 <piano4life010@gmail.com>
2024-11-28 12:31:25 -0800

    tried to fix, still doesnt seem to be working

--- Code Quest Academy/Apollo13.cpp
+++ Code Quest Academy/Apollo13.cpp
@@ -24,19 +24,14 @@ string formatNumber(double num);
 void getOutput() {
 	double x, y, z;
 	cin >> x >> y >> z;
-
-	x = fmod((x-180), 360);
-	x = x > 0 ? x : 360 + x;
-	y = fmod((y-180), 360);
-	y = y > 0 ? y : 360 + y;
-	z = fmod((z-180), 360);
-	z = z > 0 ? z : 360 + z;
-
 	cout << formatNumber(x) << formatNumber(y) << formatNumber(z) << "\n";
 }
 
 string formatNumber(double num) {
+	num = fmod((num+180), 360);
+	num = num > 0 ? num : 360 + num;
 	num = num == 360 ? 0 : num;
+
     	ostringstream oss;
     	oss << fixed << setprecision(2) << setfill('0') << setw(6) << num;
     	string formatted = oss.str();
