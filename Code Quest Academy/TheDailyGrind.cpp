cb292399ba957ca8402106de72182203241cbe90
CoolBro4811 <121374566+CoolBro4811@users.noreply.github.com>
2024-12-10 17:27:28 +0000

    starting on this is rocket science

---
+++ Code Quest Academy/TheDailyGrind.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void getOutput(int cubes, int xi, int yi);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) {	
+		int cubes, xi, yi;
+		cin >> cubes >> xi >> yi;
+		getOutput(cubes, xi, yi);
+	}
+
+	return 0;
+}
+
+void getOutput(int cubes, int xi, int yi) {
+	// starting at xi, yi
+	for (int i = 0; i < cubes; i++) {
+		int xf, yf;
+		double time;
+		cin >> xf >> yf >> time;
+
+		double dx = (xf-xi)/2.0;
+		double dy = (yf-yi)/2.0;
+
+		const double a = 1.0;
+
+	}
+}
+
