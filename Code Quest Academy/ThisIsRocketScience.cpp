38cf553a9a009a7f4161ecb0052b09bc49b97ccc
CoolBro4811 <121374566+CoolBro4811@users.noreply.github.com>
2024-12-11 23:19:44 +0000

    modified:   Code Quest Academy/ThisIsRocketScience.cpp 	modified:   Code Quest Academy/a.out 	new file:   USACO_Guide/MontmortNumber.cpp 	new file:   USACO_Guide/Template.cpp

--- Code Quest Academy/ThisIsRocketScience.cpp
+++ Code Quest Academy/ThisIsRocketScience.cpp
@@ -22,21 +22,31 @@ int main() {
 
 void getOutput(int cubes, int xi, int yi) {
 	// starting at xi, yi
+	double burnX = 0;
+	double burnY = 0;
 	for (int i = 0; i < cubes; i++) {
 		int xf, yf;
 		double time;
 		cin >> xf >> yf >> time;
 
-		double dx = (xf-xi)/2.0;
-		double dy = (yf-yi)/2.0;
+		double dx = (xf-xi)/2;
+		double dy = (yf-yi)/2;
+		time /= 2.0;
 
 		// -0.5t^2 + ti*t - dx
 		// t + sqrt(t^2 + 2*dx) /	
 		const double a = 1.0;
 
-		cout << ((2*time + sqrt(pow(time, 2) - 2*dy) + sqrt(pow(time, 2) - 2*dx))*2) << "\n";
+		burnX += 2*(time - sqrt(pow(time, 2) - 2*dx));
+		burnY += 2*(time - sqrt(pow(time, 2) - 2*dy)); 
+
+		// cout << ((2*time - sqrt(pow(time, 2) - 2*dy) - sqrt(pow(time, 2) - 2*dx))*2) << "\n";
+		
+		printf("time: %f\nxi %d, xf, %d, yi, %d, yf %d\nburnX: %lf\nburnY: %lf\n", time, xi, xf, yi, yf, burnX, burnY);
+		
 		xi = xf;
 		yi = yf;
 	}
+	cout << (burnX + burnY) << "\n";
 }
 
