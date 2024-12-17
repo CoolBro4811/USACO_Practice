306d82b404338bb4e8e57d412d930becf7f5d4d7
CoolBro4811 <121374566+CoolBro4811@users.noreply.github.com>
2024-12-17 19:26:41 +0000

    finished ThisIsRocketScience

--- Code Quest Academy/ThisIsRocketScience.cpp
+++ Code Quest Academy/ThisIsRocketScience.cpp
@@ -5,7 +5,7 @@ void getOutput(int cubes, int xi, int yi);
 
 int main() {
 	ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    cin.tie(NULL);
 	
 	int n;
 	cin >> n;
@@ -29,20 +29,16 @@ void getOutput(int cubes, int xi, int yi) {
 		double time;
 		cin >> xf >> yf >> time;
 
-		double dx = abs(xf-xi)/2;
-		double dy = abs(yf-yi)/2;
+		double dx = abs(xf-xi)/2.0; // account for half the distance, in half the time
+		double dy = abs(yf-yi)/2.0;
 		time /= 2.0;
 
 		// -0.5t^2 + ti*t - dx
-		// t + sqrt(t^2 + 2*dx) /	
+		// t + sqrt(t^2 + 2*dx)
+		// ti + sqrt(ti^2-2*dx)
 		const double a = 1.0;
-
-		burnX += 2*(time - sqrt(pow(time, 2) - 2*dx));
-		burnY += 2*(time - sqrt(pow(time, 2) - 2*dy)); 
-
-		// cout << ((2*time - sqrt(pow(time, 2) - 2*dy) - sqrt(pow(time, 2) - 2*dx))*2) << "\n";
-		
-		printf("time: %f\nxi %d, xf, %d, yi, %d, yf %d\nburnX: %lf\nburnY: %lf\n", time, xi, xf, yi, yf, burnX, burnY);
+		burnX += 2*(time - sqrt(time*time - 2*dx));
+		burnY += 2*(time - sqrt(time*time - 2*dy)); 
 		
 		xi = xf;
 		yi = yf;
