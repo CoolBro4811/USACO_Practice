c5978e41bca11b46f068d233b1b971376686499b
CoolBro4811 <piano4life010@gmail.com>
2025-01-15 11:09:10 -0800

    fixed format

--- Code Quest Academy/DonorSort.cpp
+++ Code Quest Academy/DonorSort.cpp
@@ -4,7 +4,6 @@ using namespace std;
 void getOutput();
 
 int main() {
-	cout << "start" << endl;
 	ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 	
@@ -13,7 +12,6 @@ int main() {
 	cin.ignore();
 
 	for (int i = 0; i < n; i++) {
-		cout << "loop " << i << endl;
 		getOutput();
 	}
 
@@ -21,31 +19,23 @@ int main() {
 }
 
 void getOutput() {
-cout << "start output" << endl;
 	vector<string> lastDonors, thisDonors;
 	string temp;
 	getline(cin, temp);
 	int index = 0;
 	while (index != -1) {
-		cout << "loop output while " << index << endl;
 		index = temp.find(',');
 		lastDonors.push_back(temp.substr(0, index));
 		temp = temp.substr(index+1, temp.length()-1-index);
-		cout << temp << "\n";
 	}
-	for (vector<string>::const_iterator i = lastDonors.begin(); i != lastDonors.end(); ++i) cout << *i << endl;
-
 
 	getline(cin, temp);
 	index = 0;
 	while (index != -1) {
-		cout << "loop 2 output while " << index << endl;
 		index = temp.find(',');
 		thisDonors.push_back(temp.substr(0, index));
 		temp = temp.substr(index+1, temp.length()-1-index);
-		cout << temp << "\n";
 	}
-	for (vector<string>::const_iterator i = thisDonors.begin(); i != thisDonors.end(); ++i) cout << *i << "\n";
 
 	string both = "";
 	string onlylast = "";
@@ -64,12 +54,11 @@ cout << "start output" << endl;
 		}
 	}
 
-	cout << onlylast.substr(0, onlylast.begin()-onlylast.end()-1) << "\n" << both.substr(0, both.begin()-both.end()-1) << "\n";
+	cout << onlylast.substr(0, onlylast.end()-onlylast.begin()-1) << "\n" << both.substr(0, both.end()-both.begin()-1) << "\n";
 
 	for (int i = 0; i < thisDonors.size(); i++) {
 		onlythis += thisDonors[i] + ",";
 	}
 
-	cout << onlythis.substr(0, onlythis.begin()-onlythis.end()-1) << "\n";
-	cout << "Done\n";
+	cout << onlythis.substr(0, onlythis.end()-onlythis.begin()-1) << "\n";
 }
