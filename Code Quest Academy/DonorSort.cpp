b06427df4f5dbb590921354ee49a12bfef31c685
CoolBro4811 <piano4life010@gmail.com>
2025-01-15 10:22:05 -0800

    fixed logic, need to format and remove bug couts

--- Code Quest Academy/DonorSort.cpp
+++ Code Quest Academy/DonorSort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void getOutput();
 
 int main() {
+	cout << "start" << endl;
 	ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 	
@@ -12,6 +13,7 @@ int main() {
 	cin.ignore();
 
 	for (int i = 0; i < n; i++) {
+		cout << "loop " << i << endl;
 		getOutput();
 	}
 
@@ -19,38 +21,55 @@ int main() {
 }
 
 void getOutput() {
+cout << "start output" << endl;
 	vector<string> lastDonors, thisDonors;
 	string temp;
 	getline(cin, temp);
 	int index = 0;
-	while (index = temp.find(',')) {
+	while (index != -1) {
+		cout << "loop output while " << index << endl;
+		index = temp.find(',');
 		lastDonors.push_back(temp.substr(0, index));
 		temp = temp.substr(index+1, temp.length()-1-index);
+		cout << temp << "\n";
 	}
+	for (vector<string>::const_iterator i = lastDonors.begin(); i != lastDonors.end(); ++i) cout << *i << endl;
+
 
 	getline(cin, temp);
 	index = 0;
-	while (index = temp.find(',')) {
+	while (index != -1) {
+		cout << "loop 2 output while " << index << endl;
+		index = temp.find(',');
 		thisDonors.push_back(temp.substr(0, index));
 		temp = temp.substr(index+1, temp.length()-1-index);
+		cout << temp << "\n";
 	}
+	for (vector<string>::const_iterator i = thisDonors.begin(); i != thisDonors.end(); ++i) cout << *i << "\n";
+
 	string both = "";
 	string onlylast = "";
 	string onlythis = "";
 
 
 	for (int i = 0; i < lastDonors.size(); i++) {
-		bool lastYear = false;
-		for (int j = 0; j < thisDonors.size(); j++) {
-			if (lastDonors[i] == thisDonors[j]) lastYear = true; break; thisDonors.erase(thisDonors.begin() + j);
+		vector<string>::iterator index = find(thisDonors.begin(), thisDonors.end(), lastDonors[i]);
+		if (index != thisDonors.end()) {
+			// found
+			both += lastDonors[i] + ",";
+			thisDonors.erase(index);
 		}
-		if (lastYear) {both += lastDonors[i] + ","; continue;
-		} else onlylast += lastDonors[i] + ","; continue;
+		else {
+			onlylast += lastDonors[i] + ",";
+		}
 	}
-	cout << onlylast << "\n" << both << "\n";
+
+	cout << onlylast.substr(0, onlylast.begin()-onlylast.end()-1) << "\n" << both.substr(0, both.begin()-both.end()-1) << "\n";
+
 	for (int i = 0; i < thisDonors.size(); i++) {
 		onlythis += thisDonors[i] + ",";
 	}
-	cout << onlythis.substr(0, onlythis.begin()-onlythis.end()-1) << "\n";
-}
 
+	cout << onlythis.substr(0, onlythis.begin()-onlythis.end()-1) << "\n";
+	cout << "Done\n";
+}
